eafef6cbb391ecbd73d756dd15647ddd0604638d
Patrick McGraw <patrick@mcgraw-tech.com>
2013-07-18 14:00:59 -0700

    delay loading audited until after rails-observers has had time to setup

--- lib/audited-activerecord.rb
+++ lib/audited-activerecord.rb
@@ -1,2 +1,14 @@
-require 'audited'
-require 'audited/adapters/active_record'
+require 'rails/railtie'
+
+module Audited
+  module ActiveRecord
+    class Railtie < ::Rails::Railtie
+
+      config.after_initialize do |app|
+        require 'audited'
+        require 'audited/adapters/active_record'
+      end
+
+    end
+  end
+end
