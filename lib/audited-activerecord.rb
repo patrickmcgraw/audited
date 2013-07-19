33246c572839d7591fe35053d2b92a683ae1bd47
Patrick McGraw <patrick@mcgraw-tech.com>
2013-07-19 13:17:30 -0700

    more tweaks to the railtie

--- lib/audited-activerecord.rb
+++ lib/audited-activerecord.rb
@@ -4,10 +4,11 @@ module Audited
   module ActiveRecord
     class Railtie < ::Rails::Railtie
 
-      #config.after_initialize do |app|
-      ActiveSupport.on_load(:active_record) do
-        require 'audited'
-        require 'audited/adapters/active_record'
+      initialize "audited.activerecord", after: "active_record.observer" do
+        ActiveSupport.on_load(:active_record) do
+          require 'audited'
+          require 'audited/adapters/active_record'
+        end
       end
 
     end
