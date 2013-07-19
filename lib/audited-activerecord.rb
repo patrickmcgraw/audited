64da9ee3449b33358d912fefbdfeeaa95b0ff80f
Patrick McGraw <patrick@mcgraw-tech.com>
2013-07-19 13:10:11 -0700

    setup audited on active_record load rather than waiting for after_initialize

--- lib/audited-activerecord.rb
+++ lib/audited-activerecord.rb
@@ -4,7 +4,8 @@ module Audited
   module ActiveRecord
     class Railtie < ::Rails::Railtie
 
-      config.after_initialize do |app|
+      #config.after_initialize do |app|
+      ActiveSupport.on_load(:active_record) do
         require 'audited'
         require 'audited/adapters/active_record'
       end
